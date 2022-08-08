9c61a0693bfa55304ee055c041a63ad5304aaa4f
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-08-08 03:12:42 +0200

    Create 8_Sqrt(x).cpp

---
+++ 8_Sqrt(x).cpp
@@ -0,0 +1,15 @@
+//Problem Description : https://leetcode.com/problems/sqrtx/
+class Solution {
+public:
+    int mySqrt(int x) {
+            int counter = 0;
+    for (int i = 1; x > 0; i = i + 2, counter++)
+        x -= i;
+
+    if (x < 0)
+        return counter - 1;
+    return counter;
+
+        
+    }
+};
