61a07a946920bc5ccd2babb07a2f72a0ff3d33f2
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-08-19 14:25:02 +0200

    Time: 34 ms (51.52%), Space: 5.9 MB (72.69%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+     
+        if(n<=2147483647 &&n >= -2147483647 &&n>0 )
+            return (1162261467 % n == 0)  ;
+
+            return false ;
+    }
+};
