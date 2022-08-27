5ad4921c132de469dea597aed1fceb4e9e0774b2
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-08-27 19:40:35 +0200

    Time: 40 ms (36.51%), Space: 24 MB (7.82%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+            vector<int> even;
+    vector<int> odd;
+    vector<int> result;
+    int temp = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        temp = nums[i];
+        if (temp % 2 == 0)
+            even.push_back(temp);
+        else
+            odd.push_back(temp);
+    }
+    for (int i = 0; i < nums.size(); i++)
+    {
+         if (i % 2 == 0)
+        {
+            result.push_back(even.back());
+            even.pop_back();
+        }
+        else
+        {
+
+            result.push_back(odd.back());
+            odd.pop_back();
+        }
+    }
+    return result;
+
+    }
+};
