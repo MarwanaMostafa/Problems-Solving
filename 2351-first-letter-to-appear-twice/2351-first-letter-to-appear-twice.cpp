38469304a3fb5e87b1a42a3d91945985e68e4ad8
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-19 04:31:21 +0200

    Time: 0 ms (100.00%), Space: 6.2 MB (59.46%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -3,13 +3,12 @@ public:
     char repeatedCharacter(string s) {
             map<char, int> countchar;
     for (int i = 0; i < s.length(); i++)
-        countchar.insert({s[i], 0});
-    for (int i = 0; i < s.length(); i++)
-    {
-         countchar[s[i]]++;
+    {        
+        countchar.insert({s[i], countchar[s[i]]++});
         if (countchar[s[i]] == 2)
             return s[i];
-    }return ' ';
+    }
+     return ' ';
 
     }
 };
