bae4de9613a9f486dff6ee19224a58583787aa72
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-20 08:04:26 +0200

    Time: 47 ms (5.74%), Space: 15.9 MB (17.12%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
       
-    unordered_map <int, int> count;
+    map<int, int> count;
     vector<int> result;
 
     for (int i = 0; i < nums.size(); i++)
         count.insert({nums[i], count[nums[i]]++});
-    for (unordered_map <int, int>::iterator itr = count.begin(); itr != count.end(); ++itr)
+    for (map<int, int>::iterator itr = count.begin(); itr != count.end(); ++itr)
         if (itr->second > nums.size() / 3)
             result.push_back(itr->first);
 
