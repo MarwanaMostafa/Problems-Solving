622e43965b9346cdac0448915fb39c9cb0e54f53
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-20 08:17:54 +0200

    Time: 154 ms (74.67%), Space: 38.7 MB (63.92%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        map<int, int> count;
+    int result = -1;
+    int counter=-1;
+
+    for (int i = 0; i < nums.size(); i++)
+        if (nums[i] % 2 == 0)
+            count.insert({nums[i], count[nums[i]]++});
+
+    for (map<int, int>::iterator itr = count.begin(); itr != count.end(); ++itr)
+        if (itr->second > counter)
+        { 
+            counter=itr->second;
+            result = itr->first;
+        
+        }
+    return result;
+    }
+};
