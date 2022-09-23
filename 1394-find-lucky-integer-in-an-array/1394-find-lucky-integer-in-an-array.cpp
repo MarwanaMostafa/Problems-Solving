3ca81730413ef7fbc2d02324faa3e9587c795c7f
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-24 01:26:01 +0200

    Time: 7 ms (84.85%), Space: 10.6 MB (6.81%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
@@ -6,6 +6,7 @@ public:
     int luckyNumber = -1;
     for (int i = 0; i < arr.size(); i++)
         count[arr[i]]++;
+
     for (auto i : count)
         if (i.first == i.second)
             luckyNumber = i.second;
