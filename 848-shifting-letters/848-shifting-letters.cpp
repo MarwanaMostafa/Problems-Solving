6734da40b6f70ceac5e072e5c58f17ae2ded2d24
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-26 01:51:55 +0200

    Time: 449 ms (5.13%), Space: 67.8 MB (85.17%) - LeetHub

---
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+    int sum = 0;
+    for (int i = 0; i < shifts.size(); i++)
+    {   if(shifts[i]>26)
+            shifts[i]%=26;
+        sum += shifts[i];
+    }
+    for (int i = 0; i < shifts.size(); i++)
+    {
+        if ((s[i] + (sum % 26)) > 122)
+            s[i] = (s[i] + (sum % 26)) - 26;
+        else
+            s[i] = (s[i] + (sum % 26));
+        sum -= shifts[i];
+    }
+    return s;
+    }
+};
