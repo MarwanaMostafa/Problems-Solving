6c51ca7461e36d1c7a163f00c1ee2fc7d853be5f
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-26 02:56:49 +0200

    Time: 7 ms (94.68%), Space: 12 MB (22.65%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPrefixes(vector<string>& words, string s) {
-        int count = 0;
+    int count = 0;
     string temp = "";
     for (int i = 0; i < s.length(); i++)
     {
