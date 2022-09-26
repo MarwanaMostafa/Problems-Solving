f88d6bcfd44f2f0256ecfe3145d9cb8bfa67a015
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-26 03:04:46 +0200

    Time: 41 ms (5.25%), Space: 9.9 MB (74.68%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+       int count = 0;
+    int flag = 0;
+    string temp = "";
+    for (int i = 0; i < words.size(); i++)
+    {
+        for (int ii = 0; ii < words[i].length() && ii < pref.length(); ii++)
+        {
+            if (words[i][ii] != pref[ii])
+                break;
+            flag++;
+        }
+        if (flag == pref.length())
+            count++;
+        flag=0;
+    }
+    return count; 
+    }
+};
