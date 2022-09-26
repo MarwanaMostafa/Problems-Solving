0a144f1ed9146fa5b3b0abab1e1bf4838f751983
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-26 21:09:08 +0200

    Time: 284 ms (6.75%), Space: 67.2 MB (21.79%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-         int counter = 0;
+    int counter = 0;
     for (int i = 0; i < nums.size(); i++)
         for (int j = 0; j < nums.size(); j++)
             if (i != j && nums[i] + nums[j] == target)
-                counter++ ;       
+                counter++;       
     return counter;
     }
 };
