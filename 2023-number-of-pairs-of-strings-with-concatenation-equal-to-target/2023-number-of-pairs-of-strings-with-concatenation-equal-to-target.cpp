01229c4b81b9fec36073ff9d9e9b91643a649a9a
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-26 21:08:54 +0200

    Time: 147 ms (44.47%), Space: 67.2 MB (21.79%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -5,8 +5,7 @@ public:
     for (int i = 0; i < nums.size(); i++)
         for (int j = 0; j < nums.size(); j++)
             if (i != j && nums[i] + nums[j] == target)
-                counter++;
-            
+                counter++ ;       
     return counter;
     }
 };
