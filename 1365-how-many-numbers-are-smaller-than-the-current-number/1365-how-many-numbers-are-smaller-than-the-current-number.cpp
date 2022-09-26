9a718892db0294299b3440bb0b0fe1bf8da7c900
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-26 21:45:53 +0200

    Time: 138 ms (5.11%), Space: 10.2 MB (36.87%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -8,7 +8,6 @@ public:
     for (int i = 0; i < nums.size(); i++)
     {
         counter=0;
-        
         for (int j = 0; j < nums.size(); j++)
             if (nums[i] > nums[j])
                 counter++;
