5f5858d9f329230b9748facaf1fff8581f149748
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-28 06:29:40 +0200

    Time: 3 ms (77.09%), Space: 6.7 MB (28.54%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+       
+    map<char, int> check;
+    for (int i = 0; i < sentence.length(); i++)
+        check[sentence[i]]++;
+    if (check.size() == 26)
+        return true;
+    return false ;
+    }
+};
