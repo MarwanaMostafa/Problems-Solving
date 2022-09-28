2e2cdd74ce533eca2d5ad7b6ceb37345e3f447df
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-28 06:42:37 +0200

    Time: 9 ms (18.17%), Space: 6.8 MB (5.83%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -4,13 +4,9 @@ public:
        
     map<char, int> check;
     for (int i = 0; i < sentence.length(); i++)
-        check[sentence[i]];
-    
+        check[sentence[i]];  
     if (check.size() == 26)
-        return true;      
-  
-        
-        
+        return true;     
         
         
     return false ;
