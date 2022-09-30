c4130a637ee7e9e8e9ca3b28bc54c38e6d837b6a
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-30 14:55:30 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (83.55%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int maximum69Number (int num) {
+    if(num==9999)   
+        return num;
         
-   
     string x = to_string(num);
     for (int i = 0; i < 4; i++)
         if (x[i] == '6')
