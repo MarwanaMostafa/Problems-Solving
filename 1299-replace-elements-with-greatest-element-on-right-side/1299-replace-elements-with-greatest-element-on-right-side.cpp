ecb7fd9ef24844e74a0cb3dfb07d9d546a9c039d
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-30 15:21:12 +0200

    Time: 26 ms (65.35%), Space: 14.7 MB (23.35%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-    int  temp,mx=-1;
-          
+    int  temp,mx=-1;      
     for(int i=arr.size()-1;i>=0;i--)
     {    
         temp=arr[i];
