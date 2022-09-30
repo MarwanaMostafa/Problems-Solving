577607ccc2f6d6ee0e4b1543a8f95ee359bcefb9
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-30 02:45:37 +0200

    Time: 37 ms (24.59%), Space: 15.9 MB (70.02%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        if(target=='z')
-            return letters[0];
+        
         for(int i=0;i<letters.size();i++)
             if(letters[i]>target)
                 return letters[i];
-            return letters[0];
+        return letters[0];
     }
 };
