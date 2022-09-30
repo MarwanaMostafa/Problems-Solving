a4075ca0f2f449968ad3626e976d8f2c636f19f1
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-09-30 16:30:52 +0200

    Time: 144 ms (33.73%), Space: 32.3 MB (31.91%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -5,12 +5,13 @@ public:
     sort(arr.begin(),arr.end());
     vector<vector<int>> result;
    
-    
     int minDifference = INT_MAX;
     for (int i = 0; i < arr.size() - 1; i++)
         if (abs(arr[i + 1] - arr[i]) < minDifference)
             minDifference = abs(arr[i + 1] - arr[i]);
 
+        
+        
     for (int i = 0; i < arr.size() - 1; i++)
         if (abs(arr[i + 1] - arr[i]) == minDifference)
             result.push_back({arr[i], arr[i + 1]});
