8c7adfda17b94ee7672b7d3a2868620d0dc41b22
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-01 03:12:47 +0200

    Time: 6 ms (29.93%), Space: 8 MB (75.56%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int largest_Altitude = 0;
+    int summation = 0;
+    for (int i = 0; i < gain.size(); i++)
+    {
+        summation += gain[i];
+        if (summation > largest_Altitude)
+            largest_Altitude = summation;
+    }
+    cout<<largest_Altitude<<endl;
+    return largest_Altitude;
+    }
+};
