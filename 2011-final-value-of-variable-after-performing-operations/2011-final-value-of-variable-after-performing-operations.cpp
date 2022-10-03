aa8beed4ac1c1754e90c32591821bba85e027365
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-03 12:35:07 +0200

    Time: 8 ms (88.03%), Space: 14.1 MB (56.72%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+            int result = 0;
+    for (int i = 0; i < operations.size(); i++)
+    {
+        if (operations[i][0] == '+' || operations[i][1] == '+')
+            result++;
+        else
+            result--;
+    }
+    return result;
+
+    }
+};
