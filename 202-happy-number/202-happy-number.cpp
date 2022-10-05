b01d39c5553bcd9367e8185b41db90f0d929cde3
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-05 13:25:38 +0200

    Time: 27 ms (5.15%), Space: 6.2 MB (42.29%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        map<int, int> Isrepeated;
+    int temp = 0;
+   
+      
+    while (n != 1)
+    {
+        while (n > 0)
+        {
+            temp += (n % 10) * (n % 10);
+            cout << temp << endl;
+            n /= 10;
+        }
+        Isrepeated[temp]++;
+        if (Isrepeated[temp] > 1)
+            return false;
+        n = temp;
+        temp = 0;
+    }
+    return true;
+    }
+};
