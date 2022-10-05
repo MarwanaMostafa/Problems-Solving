9d23652c6c7ea78119f2568a6f087cbea8fb396b
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-05 15:38:22 +0200

    Time: 711 ms (12.19%), Space: 120.1 MB (27.94%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -3,8 +3,10 @@ public:
     int distributeCandies(vector<int>& candyType) {
           set<int> count;
     int len = candyType.size();
+        
     for (int i = 0; i < len; i++)
         count.insert(candyType[i]);
+    
     if ((len / 2) >= count.size())
         return count.size();
     return (len / 2);
