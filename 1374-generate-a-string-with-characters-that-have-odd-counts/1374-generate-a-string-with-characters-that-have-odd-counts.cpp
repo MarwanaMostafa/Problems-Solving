362531ee3bf1780d81d7e3094a4daa72fccc2272
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-07 02:24:26 +0200

    Time: 5 ms (28.11%), Space: 6.3 MB (27.95%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        
+    string temp(n - 1, 'a');
+    if (n % 2 != 0)
+        return (temp + 'a');
+    return (temp + 'b');
+    }
+};
