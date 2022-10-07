fd8df54d4b8021fcd580c6d393fac2cd915a604d
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-08 01:16:58 +0200

    Time: 0 ms (100.00%), Space: 6 MB (39.09%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findComplement(int num) {
-    vector<int> tempResult;
     int result = 0;
     for (int i = 0; 0 < num; i++)
     {
         if (num % 2 == 0)
-            tempResult.push_back(1);
+            result += pow(2, i) * 1;
+
         else
-            tempResult.push_back(0);
+            result += pow(2, i) * 0;
+
         num /= 2;
-        result += pow(2, i) * tempResult[i];
     }
     return result;
     }
