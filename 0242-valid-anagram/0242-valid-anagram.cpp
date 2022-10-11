ee646406ca386d19de4f042a07943f84858c1a28
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-11 20:57:31 +0200

    Time: 30 ms (18.09%), Space: 7.4 MB (55.91%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
+        
+        unordered_map<char,int>count;
+        for(int i=0;i<s.length();i++)
+            count[s[i]]++;
+        for(int i=0;i<t.length();i++)
+        {
+            if(count[t[i]]==0)
+                return false;
+            count[t[i]]--;
+        }
+                    return true;
+
+    }
+    
+};
