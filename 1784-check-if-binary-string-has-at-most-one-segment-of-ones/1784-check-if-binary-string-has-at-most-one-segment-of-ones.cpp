15429d592dfd5cc3b2a7a1ebff223e0d29c1f251
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-12 13:11:32 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (43.98%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -3,8 +3,6 @@ public:
     bool checkOnesSegment(string s) {
        int firstOccurrence1 = s.find('1');
 
-    if (firstOccurrence1 == string::npos)
-        return false;
 
     int i = firstOccurrence1;
 
