6f35be4badf9b268c8c98085afb27b0c2fb95157
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-16 09:41:38 +0200

    Time: 0 ms (100.00%), Space: 6 MB (26.07%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -3,14 +3,10 @@ public:
     int totalMoney(int n) {
         int result=0;
         int remainder=n%7;
-        if(n<7)
-        {   for(int i=1;i<=n;i++)
-                result+=i;
-        return result;
-        }
+        
         for(int i=0;i<n-remainder;i+=7)
             result+=28+i;
-        cout<<"ENTER"<<result<<endl;
+
         for(int i=(n/7)+1;i<(n/7)+1+remainder;i++)
             result+=i;
             
