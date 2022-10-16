f2d9e56e5eb23c55a84d65a1f872dd7390976af4
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-16 09:29:56 +0200

    Time: 3 ms (77.11%), Space: 7.7 MB (93.47%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -3,17 +3,20 @@ public:
     int sumOfUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int result=0;
-        if(nums.size()==1)
+        int len=nums.size();
+        if(len==1)
             return nums[0];
+        
         if(nums[0]!=nums[1])
             result+=nums[0];
-        for(int i=1;i<nums.size()-1;i++)
+        
+        for(int i=1;i<len-1;i++)
             if(nums[i]!=nums[i+1] &&nums[i]!=nums[i-1])
             {
                     result+=nums[i];
             }
-        if(nums[nums.size()-2]!=nums[nums.size()-1])
-            result+=nums[nums.size()-1];
+        if(nums[len-2]!=nums[len-1])
+            result+=nums[len-1];
         return result;
     }
 };
