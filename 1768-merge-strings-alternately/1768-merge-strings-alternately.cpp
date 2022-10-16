aeefd7f683c2b1142c276d94747a15e50f4ecc8b
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-16 20:18:15 +0200

    Time: 7 ms (17.60%), Space: 6.4 MB (18.03%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string result="";
-        int i=0;
+       
         if(word1.length()>word2.length())
         {
-            for(;i<word2.length();i++)
+            for( int i=0;i<word2.length();i++)
             {
                 result+=word1[i];
                 result+=word2[i];
             }
             
-            for(;i<word1.length();i++)
+            for( int i=word2.length();i<word1.length();i++)
                 result+=word1[i];
         }
         else{
-            for(;i<word1.length();i++)
+            for( int i=0;i<word1.length();i++)
             {
                 result+=word1[i];
                 result+=word2[i];
             }
             
-            for(;i<word2.length();i++)
+            for( int i=word1.length();i<word2.length();i++)
                 result+=word2[i];
         }
         return result;
