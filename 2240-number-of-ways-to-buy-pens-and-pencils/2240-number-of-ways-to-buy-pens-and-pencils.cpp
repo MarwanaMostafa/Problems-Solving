3ca0ceec7892c471761b719c8607fd4568140d2b
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-10-19 19:49:26 +0200

    Time: 10 ms (99.32%), Space: 5.9 MB (29.01%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -3,11 +3,21 @@ public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long long counter = 0;
     int temp = 0;
+    if(cost1>cost2){
     while (temp <= total)
     {
         counter += ((total - temp) / cost2) + 1;
         temp += cost1;
     }
+    }
+        else{
+    while (temp <= total)
+    {
+        counter += ((total - temp) / cost1) + 1;
+        temp += cost2;
+    }
+            
+        }
     return counter;
     }
 };
