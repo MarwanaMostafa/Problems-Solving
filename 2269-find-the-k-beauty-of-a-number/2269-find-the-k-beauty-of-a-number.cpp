0c5a284ba62ea52e83c84e32088e004b26d03003
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-11-02 06:59:05 +0200

    Time: 7 ms (12.43%), Space: 6 MB (17.53%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -7,8 +7,9 @@ public:
         for(int i=0;i<number.length();i++)
         {
             temp=number.substr(i,k);
-            cout<<temp<<endl;
-            if(temp.length()==k&&stoi(temp)!=0&&num%stoi(temp)==0)
+            if(temp.length()<k)
+                break;
+            if(stoi(temp)!=0&&num%stoi(temp)==0)
                 counter++;
         }
     return counter;
