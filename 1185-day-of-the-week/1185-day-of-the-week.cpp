992e6b2d52af62aed20019970ae8439e205d149b
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2022-11-09 20:56:49 +0200

    Time: 3 ms (33.07%), Space: 5.9 MB (53.28%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+            string days []= {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+           if (month < 3) month += 12, year -= 1;
+        int c = year / 100
+        ;year = year % 100;
+        int w = (c / 4 - 2 * c + year + year / 4 + 13 * (month + 1) / 5 + day - 1) % 7;
+        return days[(w + 7) % 7];
+    }
+};
