f2403d87f8c952e096e349e0fae1060a103d7cd8
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-15 00:35:53 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (5.65%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -4,7 +4,7 @@ public:
         vector<double>result;
         result.push_back(celsius+273.15);
         result.push_back((celsius*1.80)+32.00);
-        return result;
+        return {celsius+273.15,(celsius*1.80)+32.00};
 
     }
 };
