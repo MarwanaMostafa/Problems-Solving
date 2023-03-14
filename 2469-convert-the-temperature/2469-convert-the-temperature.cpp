b8116c465cdff7f5b239722c5644cd048ab976ce
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-15 00:39:05 +0200

    Time: 3 ms (39.07%), Space: 6.1 MB (65.52%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        vector<double>result;
-        result.push_back(celsius+273.15);
-        result.push_back((celsius*1.80)+32.00);
+
         return {celsius+273.15,(celsius*1.80)+32.00};
 
     }
