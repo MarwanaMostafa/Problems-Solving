579c98ea34513134656ef5f331f257de21f93553
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-16 02:42:14 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (19.12%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-  int result, temp = 0;
+  int result, temp = 0,summationN=(n*(n+1))/2;
     for (int i = 1; i <= n; i++) {
 
-        for (int j = 0; j <= i; j++)
-            result += j;
-        for (int k = i; k <= n; k++)
-            temp += k;
+        result=(i*(i+1))/2;
+        temp=summationN-result+i;
         if (result == temp)
             return i;
         temp=0, result = 0;
