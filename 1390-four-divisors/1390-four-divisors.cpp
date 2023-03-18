a82482a71fd8c5b2b86cb8ca082aaeebc114b17f
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-19 00:57:16 +0200

    Time: 29 ms (93.51%), Space: 12.5 MB (54.22%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+  
+int getDivisors(int n)
+{
+  int count =0,result=0;
+    
+  for (int i=1; i<=sqrt(n)&&count<5; i++)
+  {
+    if (n%i == 0)
+    {
+      if (n/i == i)
+      {
+        count++;
+        result+=i;
+      }
+      else
+      {
+        count+=2;
+        result+=i+n/i;
+      }
+    }
+  }
+  if(count!=4)
+    return 0;
+  return result;    
+}
+int sumFourDivisors(vector<int>& nums) {
+       int result=0;
+       for(int i=0;i<nums.size();i++)
+          result+= getDivisors(nums[i]);
+      return result;
+}
+};
