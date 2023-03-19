92e41475293c5b06d8dae069896c35a49b25ac6e
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-20 01:42:40 +0200

    Time: 20 ms (51.44%), Space: 31.5 MB (99.18%) - LeetHub

---
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool IsVowel(string word)
+    {
+        int len=word.length()-1;
+        if((word[0]=='a'||word[0]=='u'||word[0]=='o'||word[0]=='i'||word[0]=='e')
+          &&(word[len]=='a'||word[len]=='u'||word[len]=='o'||word[len]=='i'||word[len]=='e'
+          ))
+            return true;
+        return false;
+    }
+    int vowelStrings(vector<string>& words, int left, int right) {
+        int result=0;
+        while(left<=right){
+            if(IsVowel(words[left]))
+                result++;
+        left++;
+    }
+        return result;
+    }
+};
