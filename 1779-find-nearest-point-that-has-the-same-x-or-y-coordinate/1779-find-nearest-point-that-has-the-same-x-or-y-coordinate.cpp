df2eab540eb9f4c20d54c44f8e6961b85d3b1834
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-23 03:16:07 +0200

    Time: 167 ms (86.37%), Space: 59.5 MB (9.28%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -3,18 +3,13 @@ public:
     int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
         int result=100000,index=-1;
         for(int i=0;i<points.size();i++)
-        {
             if(points[i][1]==y||points[i][0]==x )
-            {
-                
                 if(result>abs(points[i][0]-x)+ abs(points[i][1]-y))
                 {
                     result=abs(points[i][0]-x)+ abs(points[i][1]-y);
                     index=i;
                 }
-            }
-            
-        }
+        
         return index;
     }
 };
