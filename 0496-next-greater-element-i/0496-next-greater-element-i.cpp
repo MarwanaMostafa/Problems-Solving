88c75381ef12bcd399d567f98223ced22ba69106
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-25 06:18:44 +0200

    Time: 28 ms (17.70%), Space: 8.4 MB (90.18%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+            vector<int> result;
+        for(int i=0;i<nums1.size();i++)
+        {
+            
+                    bool flag=false;
+            for(int j=0;j<nums2.size();j++)
+                if(nums1[i]==nums2[j])
+                {   
+                    for(int k=j+1;k<nums2.size();k++)
+                        if(nums1[i]<nums2[k])
+                        {
+                            result.push_back(nums2[k]);
+                         flag=true;
+                                break;
+                        }
+
+                    break;
+                }
+            
+            if(!flag)
+            {
+                cout<<nums1[i]<<endl;
+                result.push_back(-1);
+        }}
+        return  result;
+        
+    }
+};
