6e9f6fa283ddae3e547bdb700282110ed4246fe1
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-26 03:16:25 +0200

    Time: 0 ms (100.00%), Space: 8.2 MB (82.11%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -5,6 +5,7 @@ public:
 
         
         for (int currentSum = 0,left = 0; left < n;currentSum = 0, ++left) 
+            
             for (int right = left; right < n; ++right) {
                 currentSum += arr[right];
                 answer += (right - left + 1) % 2 == 1 ? currentSum : 0;
