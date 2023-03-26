5f01620efbf6c9abcec5db399704eaefa704928c
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-26 03:16:13 +0200

    Time: 7 ms (42.80%), Space: 8.4 MB (44.01%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-          int n = int(arr.size()), answer = 0;
+          int n = arr.size(), answer = 0;
 
         
-        for (int left = 0; left < n; ++left) {
-            int currentSum = 0; 
+        for (int currentSum = 0,left = 0; left < n;currentSum = 0, ++left) 
             for (int right = left; right < n; ++right) {
                 currentSum += arr[right];
                 answer += (right - left + 1) % 2 == 1 ? currentSum : 0;
             }
-        }
+        
         return answer;
     
     }
