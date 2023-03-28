aa33d1ec2132b551afb87fffc51b7aedae6f186c
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-28 02:45:27 +0200

    Time: 8 ms (75.12%), Space: 10.6 MB (35.05%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -15,9 +15,9 @@ public:
         for(int i=0;i<arr.size();i++)
              result.push_back( make_pair(numberOfOnes(arr[i]),arr[i]) );
         sort(result.begin(),result.end());
-        vector<int>res;
+
         for(int i=0;i<arr.size();i++)
-             res.push_back( result[i].second );
-        return res;
+             arr[i]= result[i].second ;
+        return arr;
     }
 };
