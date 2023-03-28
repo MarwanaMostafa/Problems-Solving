ce18238ee72afc9c81b0188d7276ff7847a22d4f
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-03-28 02:48:20 +0200

    Time: 12 ms (48.13%), Space: 10.7 MB (28.73%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int numberOfOnes(int number){
-        int counter=0;
-        while(number!=0){
-            if(number%2!=0)
-                counter++;
-        number/=2;
-        }
-        return counter     ;   
-    }
     
     vector<int> sortByBits(vector<int>& arr) {
         vector<pair<int,int>>result;
+      
         for(int i=0;i<arr.size();i++)
-             result.push_back( make_pair(numberOfOnes(arr[i]),arr[i]) );
+        {
+                int counter=0,number=arr[i];
+            while(number!=0){
+                 if(number%2!=0)
+                     counter++;
+                 number/=2;
+            }
+            result.push_back( make_pair(counter,arr[i]) );
+
+        }
+        
         sort(result.begin(),result.end());
 
         for(int i=0;i<arr.size();i++)
