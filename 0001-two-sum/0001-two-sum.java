912d3e9b72ecf6d510e8883e500968ecb818155a
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-08-04 22:18:08 +0300

    Time: 45 ms (51.28%), Space: 44.3 MB (6.21%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] soultion = { 0, 0 };
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    soultion[0] = i;
+                    soultion[1] = j;
+                    return soultion;
+                }
+            }
+
+        }
+        return soultion;
+    }
+}
