9207c8f829bdcf7154faa10589a004a2ac591c8b
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-08-08 12:49:33 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (56.16%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int hayLen = haystack.length();
+        int needleLen = needle.length();
+
+        if (needleLen == 0) {
+            return 0;
+        }
+
+        if (hayLen < needleLen) {
+            return -1;
+        }
+
+        for (int i = 0; i <= hayLen - needleLen; ++i) {
+            int j;
+            for (j = 0; j < needleLen; ++j) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == needleLen) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
