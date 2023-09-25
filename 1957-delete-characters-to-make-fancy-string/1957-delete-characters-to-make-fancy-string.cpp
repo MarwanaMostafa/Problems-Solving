189888ec8a3f64dcda785c812a2db6b52365676f
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-09-25 14:23:37 +0300

    Time: 128 ms (64.66%), Space: 41.4 MB (16.83%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     string makeFancyString(string s) {
+        
         string result="";
+        result+=s[0];
         int length=s.size();
         
-        for(int i=0;i<length;i++)
+        for(int i=1;i<length;i++)
         {   
-            if(s[i]==s[i+1]&&s[i+1]==s[i+2])
+            if(s[i]==s[i-1]&&s[i+1]==s[i])
                      continue;
         
             result+=s[i];
