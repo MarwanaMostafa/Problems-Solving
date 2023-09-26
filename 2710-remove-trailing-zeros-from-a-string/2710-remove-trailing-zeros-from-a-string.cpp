ba42dbd1ae0f3b58a0a457882fb797a9f06d1f74
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-09-26 13:32:23 +0300

    Time: 11 ms (58.31%), Space: 9.3 MB (29.32%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -3,18 +3,14 @@ public:
     string removeTrailingZeros(string num) {
 
         
-        string tempNum="";
         int i=num.size()-1;
         
         for(;i>=0;i--)
             if(num[i]!='0')
                 break;
 
-        for(int j=0;j<=i;j++)
-            tempNum+=num[j];
-
         
-        return  tempNum;
+        return  num.substr(0,i+1);
             
         
                 
