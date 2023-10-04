c1457158466dfec7ec7a0d98b43c96ce37aec78c
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2023-10-04 12:59:59 +0300

    Time: 3 ms (51.79%), Space: 6.3 MB (11.49%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-    time =( time % ((n - 1) * 2) ) + 1;
+    time =( time % ((n - 1) * 2) ) + 1/*we use +1 to return back again */;
     return min(time, n * 2 - time);
     }
 };
