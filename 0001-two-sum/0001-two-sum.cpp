24d19cd794d70d242a6c881b8b32a9aa0753b123
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2024-03-14 11:20:53 +0200

    Time: 10 ms (67.48%), Space: 14.1 MB (25.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); ++j) {
-                if (nums[j] == target - nums[i]) {
-                    return  { i, j };
-                }
-            }
-            
-            
-            
-            
+        unordered_map<int, int> numMap;
+    vector<int> result;
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            result.push_back(numMap[complement]);
+            result.push_back(i);
+            break;
         }
-        // In case there is no solution, we'll just return null
-        return {}; 
+        numMap[nums[i]] = i;
+    }
+    return result;
     }
 };
