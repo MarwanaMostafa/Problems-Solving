b0999d0d990eb3494a9f60707ff48701b4287ce3
marwan mostafa ghaleb ghalab <58035531+MarwanaMostafa@users.noreply.github.com>
2024-12-07 02:59:08 +0200

    Time: 1 ms (99.69%), Space: 42.2 MB (11.95%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int absDiff=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            absDiff+=Math.abs(Integer.valueOf(s.charAt(i))-Integer.valueOf(s.charAt(i+1)));
+        }
+        return absDiff;
+    }
+}
